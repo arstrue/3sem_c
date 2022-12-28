faf2cefe3aad638b5790163a3412ecc3e363eb30
NxthxnD <89542218+NxthxnD@users.noreply.github.com>
2022-12-29 00:53:11 +0300

    Update tasks6-8.cpp

--- tasks6-8.cpp
+++ tasks6-8.cpp
@@ -16,25 +16,76 @@ void test8();
 template <typename T, typename U>
 std::ostream& operator<<(std::ostream& out, const std::pair<T, U>& p)
 {
-    out << "(" << p.first << ", " << p.second << ")";
+    
+    return out << "{" << p.first << ", " << p.second << "}";
+}
+
+template <>
+std::ostream& operator<<(std::ostream& out, const std::pair<std::string, std::string>& p)
+{
+    
+    return out << '{' << '"' << p.first << '"' << ", " 
+        << '"' << p.second << '"' << '}';
+}
+
+template <>
+std::ostream& operator<<(std::ostream& out, const std::pair<char, char>& p)
+{
+    out << '{';
+    if (p.first == '\0')
+        out << "'\\0'";
+    else
+        out << "'" << p.first << "'";
+    out << ", ";
+    if (p.second == '\0')
+        out << "'\\0'";
+    else
+        out << "'" << p.second << "'";
+    out << '}';
+    return out;
+}
+
+template <typename T, int U>
+std::ostream& operator<<(std::ostream& out, const std::array<T, U>& a)
+{
+    out << '{';
+    for (size_t i = 0; i < U - 1; ++i)
+        out << a[i] << ", ";
+
+    if (U)
+        out << a[U - 1];
+
+    out << '}';
+    return out;
+}
+
+template <int U>
+std::ostream& operator<<(std::ostream& out, const std::array<std::string, U>& a)
+{
+    out << '{';
+    for (size_t i = 0; i < U - 1; ++i)
+        out << '"' << a[i] << '"' << ", ";
+
+    if (U)
+        out << '"' << a[U - 1] << '"';
+
+    out << '}';
     return out;
 }
 
 template <typename T>
 std::ostream& operator<<(std::ostream& out, const std::vector<T>& v)
 {
-    out << "[";
+    out << '{';
     for (size_t i = 0; i < v.size() - 1; ++i)
         out << v[i] << ", ";
 
     if (!v.empty())
-    {
         out << v.back();
-    }
-    out << "]";
+
+    out << '}';
     return out;
 }
-
 
 template <typename T>
 vector<pair<typename T::value_type, typename T::value_type>> pairing(const T& c)
@@ -54,30 +105,18 @@ template <typename T>
 class Manager
 {
 private:
-    T* _ptr;
+    T* _ptr = nullptr;
+
 public:
-    Manager() : _ptr(nullptr) {};
+    void allocate() { _ptr = (T*)std::malloc(sizeof(T)); }
 
-    void allocate()
-    {
-        _ptr = (T*)std::malloc(sizeof(T));
-    }
+    void construct(const T& t) { new (_ptr) T{ t }; }
 
-    void construct(const T& t)
-    {
-        new (_ptr) T(t);
-    }
+    void destruct() { _ptr->~T(); }
 
-    void destruct() {
-        _ptr->~T();
-    }
-    void deallocate() {
-        free(_ptr);
-    }
+    void deallocate() { free(_ptr); }
 
-    T& get() {
-        return *_ptr;
-    }
+    T& get() { return *_ptr; }
 };
 
 //task8
@@ -108,55 +147,47 @@ public:
 template <typename U>
 std::ostream& operator<<(std::ostream& out, Ref<U> r)
 {
-    out << *(r._ptr);
-    return out;
+    return out << *(r._ptr);
 }
 
 
 int main()
 {
     test6();
-    std::cout << "\n\n";
     test7();
-    std::cout << "\n\n";
     test8();
 }
 
 
 void test6()
 {
-    vector v{ 10, 20, 30, 40, 50, 60 };
-    cout << pairing(v) << endl;
+    vector v{ 10, 20, 30, 40, 50 };
+    cout << v << " -> " << pairing(v) << endl;
 
+    std::array<std::string, 4> a{ "cat", "dog", "mouse", "lion" };
+    cout << a << " -> " << pairing(a) << endl;
 
-    std::array<std::string, 7> a{ "cat", "dog", "mouse", "elephant", "tiget", "axolotl", "wolf" };
-    cout << pairing(a) << endl;
-
-
-    std::string s{ "Cats and dogs!" };
-    cout << pairing(s) << endl;
-    return;
+    std::string_view s{ "Hello" };
+    cout << '"' << s << '"' << " -> " << pairing(s) << endl << endl;
 }
-
-/*
-    Ïðîãðàììà äîëæíà íàïå÷àòàòü:
-    [(10, 20), (30, 40), (50, 60)]
-    [(cat, dog), (mouse, elephant), (tiget, axolotl), (wolf, )]
-    [(C, a), (t, s), ( , a), (n, d), ( , d), (o, g), (s, !)]
-*/
 
 void test7()
 {
     Manager<std::string> a;
     a.allocate();
+
     a.construct("Cats and dogs");
     a.get() += " and elephant";
     cout << a.get() << endl;
     a.destruct();
+
     a.construct("Sapere Aude");
     cout << a.get() << endl;
     a.destruct();
+
     a.deallocate();
+
+    cout << endl;
 }
 
 void toUpper(Ref<std::string> r)
@@ -167,24 +198,27 @@ void toUpper(Ref<std::string> r)
 
 void test8()
 {
-    
-
     int a = 10;
     Ref<int> ra = a;
     ra += 10;
     cout << a << " " << ra << endl;
+
     std::string s = "Cat";
     Ref<std::string> rs = s;
     rs = "Mouse";
     rs += "Elephant";
     cout << rs << endl;
     cout << s << endl;
+
     toUpper(s);
     cout << s << endl;
+ 
     std::vector<std::string> animals{ "Cat", "Dog", "Elephant", "Worm" };
     std::vector<Ref<std::string>> refs{ animals.begin(), animals.end() };
+
     for (int i = 0; i < refs.size(); ++i)
         refs[i] += "s";
+
     for (int i = 0; i < animals.size(); ++i)
         cout << animals[i] << " ";
     cout << endl;
